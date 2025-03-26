13c3e559c1161ab42cce00d970c2d7e050f56e62
BrandonLWhite <brandonlwhite@gmail.com>
2025-03-26 11:56:58 -0500

    Improve POC script logging

--- scripts/poc/inner_package/zip_in_zip_test/__init__.py
+++ scripts/poc/inner_package/zip_in_zip_test/__init__.py
@@ -1,6 +1,10 @@
 # This file represents the original module's __init__.py file that gets renamed when creating the innner ZIP.
 
-print("__init__ original")
+import logging
+
+logger = logging.getLogger(__name__ + "(__init__.py ORIGINAL)")
+
+logger.info("Hello, I am the original pacakge __init__.py")
 
 GLOBAL_VALUE_IN_INIT_ORIGINAL = "This global is defined in the original __init__.py"
 
--- scripts/poc/inner_package/zip_in_zip_test/other_module.py
+++ scripts/poc/inner_package/zip_in_zip_test/other_module.py
@@ -1,2 +1,8 @@
+import logging
+
+logger = logging.getLogger(__name__)
+
+logger.info("Load")
+
 def other_module_function():
-    print("I'm in other_module_function")
+    logger.info("I'm in other_module_function")
