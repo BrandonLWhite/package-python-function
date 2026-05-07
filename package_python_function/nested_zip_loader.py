1300f00b72ec3bea2e6debce3f3aac3126d46bd0
Alex Jackson <contact@ajaxbits.com>
2026-05-07 14:58:08 -0400

    test: add unit tests for nested_zip_loader

--- package_python_function/nested_zip_loader.py
+++ package_python_function/nested_zip_loader.py
@@ -57,7 +57,7 @@ def load_nested_zip() -> None:
             with zipfile.ZipFile(str(nested_zip_path), "r") as nested_zip:
                 nested_zip.extractall(str(staging_package_path))
 
-            # The idea here is that we don't rename the path until everything has been successfuly extracted.
+            # The idea here is that we don't rename the path until everything has been successfully extracted.
             # This is expected to be a an atomic operation.  That way, if AWS terminates us during the extraction,
             # we won't try and use the incomplete extraction.
             os.rename(str(staging_package_path), str(target_package_path))
